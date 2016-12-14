c0e16cf1a1dcda0740caaf21ced26018cf4c5177
Ankit Yadav <ankityadav8171@gmail.com>
2016-12-14 18:59:18 +0530

    File Push
    Initial release

---
+++ bin/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// check if fields passed are empty
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['subject']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// create email body and send it	
+$to = 'you@example.com'; // PUT YOUR EMAIL ADDRESS HERE
+$email_subject = "New message on yourwebsite.com from:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
+$email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nSubject: $subject\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: anything@yourwebsite.com\n";
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
